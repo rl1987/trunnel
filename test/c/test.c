40d1e87b6a60a747062a9dcae2fdaa17b1e2a796
Nick Mathewson <nickm@torproject.org>
2014-07-25 12:08:13 -0400

    Start work on C unit tests
    (Also, import tinytest)

---
+++ test/c/test.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include "valid/basic.h"
+#include "tinytest/tinytest.h"
+#include "tinytest/tinytest_macros.h"
+
+int main(int argc, char **argv)
+{
+  (void)argc;
+  (void)argv;
+  return 0;
+}
