fa63284f879f66aba886cd06d471cb209ae4b703
Nick Mathewson <nickm@torproject.org>
2014-07-25 10:53:49 -0400

    Make run_tests.sh even more foolproof

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
 TRUNNEL=`dirname $0`/../lib/CodeGen.py
+GRAMMAR=`dirname $0`/../lib/Grammar.py
 
-RUN0="python -m coverage run"
-RUN="python -m coverage run -a"
+if python -m coverage >/dev/null ; then
+  COVERAGE="python -m coverage"
+  RUN0="python -m coverage run"
+  RUN="python -m coverage run -a"
+else
+  COVERAGE="true"
+  RUN0="python"
+  RUN="python"
+fi
+
 
 # Try failing cases.
 echo >tests.log "==== no argument"
@@ -12,3 +21,7 @@ for fn in `dirname $0`/failing/*.trunnel; do
   echo >>tests.log "==== $fn"
   $RUN $TRUNNEL $fn 2>>tests.log && echo "SHOULD HAVE FAILED: $fn"
 done
+
+$COVERAGE report $TRUNNEL $GRAMMAR
+$COVERAGE annotate $TRUNNEL
+$COVERAGE annotate $GRAMMAR
