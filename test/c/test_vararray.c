33251c7272f92164b8b6314b1846081a971856e9
Nick Mathewson <nickm@torproject.org>
2014-07-30 13:54:47 -0400

    Complete vararray tests

--- test/c/test_vararray.c
+++ test/c/test_vararray.c
@@ -124,61 +124,157 @@ test_varlen_invalid(void *arg)
   varlen_free(varlen);
 }
 
-#if 0
 static void
 test_varlen_encdec(void *arg)
 {
   const uint8_t *inp;
-  uint8_t buf[128] = {0};
   varlen_t *out = NULL;
+  uint8_t buf[128];
+  size_t len;
+  uint64_t u64;
+  numbers_t *nums;
   (void)arg;
 
-  inp = ux( "01020408"
-            "0010""0020""0040""0080""0100""0200"
-            "00000400""00000800""00001000"
-            "0000000000002000"
-            "01" "0002" "00000003"
-            "0000000000000004"
-            "50" "6000" "70000000"
-            "8000000000000000" );
+  /* (We already round-tripped these in truncated. */
 
-  tt_int_op(66, ==, varlen_parse(&out, inp, 66));
-  tt_int_op(out->a8[0], ==, 1);
-  tt_int_op(out->a8[1], ==, 2);
-  tt_int_op(out->a8[2], ==, 4);
-  tt_int_op(out->a8[3], ==, 8);
-  tt_int_op(out->a16[0], ==, 16);
-  tt_int_op(out->a16[1], ==, 32);
-  tt_int_op(out->a16[2], ==, 64);
-  tt_int_op(out->a16[3], ==, 128);
-  tt_int_op(out->a16[4], ==, 256);
-  tt_int_op(out->a16[5], ==, 512);
-  tt_int_op(out->a32[0], ==, 1024);
-  tt_int_op(out->a32[1], ==, 2048);
-  tt_int_op(out->a32[2], ==, 4096);
-  tt_assert(out->a64[0] == 8192);
-  tt_int_op(out->nums[0]->i8, ==, 1);
-  tt_int_op(out->nums[0]->i16, ==, 2);
-  tt_int_op(out->nums[0]->i32, ==, 3);
-  tt_assert(out->nums[0]->i64 == 4);
-  tt_int_op(out->nums[1]->i8, ==, 0x50);
-  tt_int_op(out->nums[1]->i16, ==, 0x6000);
-  tt_int_op(out->nums[1]->i32, ==, 0x70000000);
-  tt_assert(out->nums[1]->i64 == ((uint64_t)1)<<63 );
+  /* MINIMAL. */
+  inp = ux(MINIMAL);
+  len = strlen(MINIMAL)/2;
+  tt_int_op(len, ==, varlen_parse(&out, inp, len));
+  tt_int_op(0, ==, out->len1);
+  tt_int_op(0, ==, out->len2);
+  tt_int_op(0, ==, out->len3);
+  tt_int_op(0, ==, out->len4);
+  tt_assert(out->str);
+  tt_str_op("", ==, out->str);
+  tt_int_op(0, ==, varlen_get_a8_len(out));
+  tt_int_op(0, ==, varlen_get_a16_len(out));
+  tt_int_op(0, ==, varlen_get_a32_len(out));
+  tt_int_op(0, ==, varlen_get_a64_len(out));
+  tt_int_op(0, ==, varlen_get_nums_len(out));
+  varlen_free(out); out = NULL;
 
-  tt_int_op(66, ==, varlen_encode(buf, sizeof(buf), out));
-  tt_mem_op(buf, ==, inp, 66);
+  /* SOME_LEN1. */
+  inp = ux(SOME_LEN1);
+  len = strlen(SOME_LEN1)/2;
+  tt_int_op(len, ==, varlen_parse(&out, inp, len));
+  tt_int_op(2, ==, out->len1);
+  tt_int_op(0, ==, out->len2);
+  tt_int_op(0, ==, out->len3);
+  tt_int_op(0, ==, out->len4);
+  tt_str_op("Uf", ==, out->str);
+  tt_int_op(2, ==, varlen_get_a8_len(out));
+  tt_int_op(0, ==, varlen_get_a16_len(out));
+  tt_int_op(0, ==, varlen_get_a32_len(out));
+  tt_int_op(0, ==, varlen_get_a64_len(out));
+  tt_int_op(0, ==, varlen_get_nums_len(out));
+  tt_int_op(0x77, ==, varlen_get_a8(out, 0));
+  tt_int_op(0x88, ==, varlen_get_a8(out, 1));
+  varlen_free(out); out = NULL;
+
+  /* SOME_LEN1_SOME_LEN2. */
+  inp = ux(SOME_LEN1_SOME_LEN2);
+  len = strlen(SOME_LEN1_SOME_LEN2)/2;
+  tt_int_op(len, ==, varlen_parse(&out, inp, len));
+  tt_int_op(2, ==, out->len1);
+  tt_int_op(2, ==, out->len2);
+  tt_int_op(0, ==, out->len3);
+  tt_int_op(0, ==, out->len4);
+  tt_str_op("Uf", ==, out->str);
+  tt_int_op(2, ==, varlen_get_a8_len(out));
+  tt_int_op(2, ==, varlen_get_a16_len(out));
+  tt_int_op(0, ==, varlen_get_a32_len(out));
+  tt_int_op(0, ==, varlen_get_a64_len(out));
+  tt_int_op(2, ==, varlen_get_nums_len(out));
+  tt_int_op(0x77, ==, varlen_get_a8(out, 0));
+  tt_int_op(0x88, ==, varlen_get_a8(out, 1));
+  tt_int_op(0x1337, ==, varlen_get_a16(out, 0));
+  tt_int_op(0x1337, ==, varlen_get_a16(out, 1));
+  tt_int_op(1, ==, varlen_get_nums(out, 0)->i8);
+  tt_int_op(2, ==, varlen_get_nums(out, 0)->i16);
+  tt_int_op(3, ==, varlen_get_nums(out, 0)->i32);
+  tt_int_op(4, ==, varlen_get_nums(out, 0)->i64);
+  tt_int_op(5, ==, varlen_get_nums(out, 1)->i8);
+  tt_int_op(6, ==, varlen_get_nums(out, 1)->i16);
+  tt_int_op(7, ==, varlen_get_nums(out, 1)->i32);
+  tt_int_op(8, ==, varlen_get_nums(out, 1)->i64);
+  varlen_free(out); out = NULL;
+
+  /* SOME_LEN3. */
+  inp = ux(SOME_LEN3);
+  len = strlen(SOME_LEN3)/2;
+  tt_int_op(len, ==, varlen_parse(&out, inp, len));
+  tt_int_op(0, ==, out->len1);
+  tt_int_op(0, ==, out->len2);
+  tt_int_op(4, ==, out->len3);
+  tt_int_op(0, ==, out->len4);
+  tt_str_op("", ==, out->str);
+  tt_int_op(0, ==, varlen_get_a8_len(out));
+  tt_int_op(0, ==, varlen_get_a16_len(out));
+  tt_int_op(4, ==, varlen_get_a32_len(out));
+  tt_int_op(0, ==, varlen_get_a64_len(out));
+  tt_int_op(0, ==, varlen_get_nums_len(out));
+  tt_int_op(0x606, ==, varlen_get_a32(out, 0));
+  tt_int_op(0x0842, ==, varlen_get_a32(out, 1));
+  tt_int_op(0x867, ==, varlen_get_a32(out, 2));
+  tt_int_op(0x5309, ==, varlen_get_a32(out, 3));
+  varlen_free(out); out = NULL;
+
+  /* SOME_LEN4. */
+  inp = ux(SOME_LEN4);
+  len = strlen(SOME_LEN4)/2;
+  tt_int_op(len, ==, varlen_parse(&out, inp, len));
+  tt_int_op(0, ==, out->len1);
+  tt_int_op(0, ==, out->len2);
+  tt_int_op(0, ==, out->len3);
+  tt_int_op(2, ==, out->len4);
+  tt_str_op("", ==, out->str);
+  tt_int_op(0, ==, varlen_get_a8_len(out));
+  tt_int_op(0, ==, varlen_get_a16_len(out));
+  tt_int_op(0, ==, varlen_get_a32_len(out));
+  tt_int_op(2, ==, varlen_get_a64_len(out));
+  tt_int_op(0, ==, varlen_get_nums_len(out));
+  u64 = varlen_get_a64(out, 0);
+  tt_int_op(0x606, ==, (uint32_t)(u64 >> 32));
+  tt_int_op(0x842, ==, (uint32_t)u64);
+  u64 = varlen_get_a64(out, 1);
+  tt_int_op(0x867, ==, (uint32_t)(u64 >> 32));
+  tt_int_op(0x5309, ==, (uint32_t)u64);
+  varlen_free(out); out = NULL;
+
+  /* Now make a purely synthetic one, mainly to execute *_set() */
+  out = varlen_new();
+  out->len1 = 1;
+  out->len2 = 1;
+  out->len3 = 1;
+  out->len4 = 1;
+  out->str = strdup("Y");
+  varlen_add_a8(out, 0);
+  varlen_add_a16(out, 0);
+  varlen_add_a32(out, 0);
+  varlen_add_a64(out, 0);
+  varlen_add_nums(out, NULL);
+  varlen_set_a8(out, 0, 0x12);
+  varlen_set_a16(out, 0, 0x1234);
+  varlen_set_a32(out, 0, 0x12345678);
+  varlen_set_a64(out, 0, (((uint64_t)0x12345678)<<32)|0x9abcdef0);
+  nums = numbers_new();
+  varlen_set_nums(out, 0, nums);
+  memset(buf, 99, sizeof(buf));
+  tt_int_op(46, ==, varlen_encode(buf, sizeof(buf), out));
+  inp = ux( "01""0001""00000001""0000000000000001"
+            "59""12""1234""12345678"
+            "123456789abcdef0"
+            "00""0000""00000000""0000000000000000");
+  tt_mem_op(buf, ==, inp, 46);
 
  end:
   varlen_free(out);
 }
-#endif
 
 struct testcase_t vararray_tests[] = {
   { "truncated", test_varlen_truncated, 0, NULL, NULL },
   { "invalid", test_varlen_invalid, 0, NULL, NULL },
-#if 0
- { "encode-decode", test_varlen_encdec, 0, NULL, NULL },
-#endif
+  { "encode-decode", test_varlen_encdec, 0, NULL, NULL },
   END_OF_TESTCASES
 };
