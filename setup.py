9bcdde97124f298fc29fbf21567769c93890b3f2
Nick Mathewson <nickm@torproject.org>
2014-08-11 08:55:06 -0400

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(name='Trunnel',
+      version='1.0',
+      description='Trunnel binary format parser',
+      author='Nick Mathewson',
+      author_email='nickm@torproject.org',
+      url='https://gitweb.torproject.org/trunnel.git/',
+      package_dir = { '' : 'lib' },
+      packages=['trunnel'],
+      license='3-clause BSD'
+     )
+
